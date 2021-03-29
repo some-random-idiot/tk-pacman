d7a3f8712d5545a3c124d9b338c444962233ff5f
Sittipatty <sittipat2554aa@hotmail.com>
2021-03-29 16:06:17 +0700

    Implemented flyweight pattern.

--- maze.py
+++ maze.py
@@ -1,9 +1,10 @@
 from gamelib import Sprite
 from dir_consts import *
+import tkinter as tk
 
 class Dot(Sprite):
-    def __init__(self, app, x, y):
-        super().__init__(app, 'images/dot.png', x, y)
+    def __init__(self, app, x, y, photo_image=None):
+        super().__init__(app, 'images/dot.png', x, y, photo_image=photo_image)
 
         self.is_eaten = False
 
@@ -13,8 +14,8 @@ class Dot(Sprite):
 
 
 class Wall(Sprite):
-    def __init__(self, app, x, y):
-        super().__init__(app, 'images/wall.png', x, y)
+    def __init__(self, app, x, y, photo_image=None):
+        super().__init__(app, 'images/wall.png', x, y, photo_image=photo_image)
 
 
 class Maze:
@@ -62,12 +63,15 @@ class Maze:
             for j in range(self.get_width()):
                 x, y = self.piece_center(i, j)
 
+                self.wall_image = tk.PhotoImage(file='images/wall.png')
+                self.dot_image = tk.PhotoImage(file='images/dot.png')
+
                 if self.has_wall_at(i, j):
-                    wall = Wall(self.app, x, y)
+                    wall = Wall(self.app, x, y, photo_image=self.wall_image)
                     self.walls.append(wall)
 
                 if self.has_dot_at(i, j):
-                    dot = Dot(self.app, x, y)
+                    dot = Dot(self.app, x, y, photo_image=self.dot_image)
                     self.dots[(i,j)] = dot
 
     def __init__(self, app, canvas_width, canvas_height):
